9dcf1b43b117d2cf3017ccf246010b86326f8a18
vaibhav4399 <vaibhavprabhu19@gmail.com>
2024-09-28 19:26:50 +0530

    Add Footer Section
    Add the contents to the footer section and configure styles in index

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,8 +3,11 @@ import './Footer.css';
 
 const Footer = () => {
     return (
-        <>
-        </>
+        <div className='footer_container'>
+            <div className='footer_section'>
+                <p>Copyright Empeiros @ 2024</p>
+            </div>
+        </div>
     )
 }
 
